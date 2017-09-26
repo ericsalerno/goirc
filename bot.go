591c063819260dc1093b5d6a350ead68e3eb9e53
Eric Salerno <eric.salerno@viacom.com>
2017-09-26 16:44:34 -0400

    First connection actually works. Add size to buffer for read. Add sendRawCommand function

--- bot.go
+++ bot.go
@@ -8,6 +8,7 @@ import (
 // Bot - Main bot class
 type Bot struct {
 	config Configuration
+	server net.Conn
 }
 
 // Connect - connect to a server
@@ -25,16 +26,20 @@ func (b Bot) serverPump() {
 	fmt.Printf("Connecting to %s...\n", serverConnection)
 
 	conn, err := net.Dial("tcp", serverConnection)
+	b.server = conn
 
 	if err != nil {
 		fmt.Printf("Failed to connect to " + serverConnection)
 		return
 	}
 
+	b.sendRawCommand("USER", b.config.Nickname+" ericsalerno/goirc_1.0 "+b.config.Nickname+" :"+b.config.RealName)
+	b.sendRawCommand("NICK", b.config.Nickname)
+
 	shouldLoop := true
 	for shouldLoop {
-		var data []byte
-		_, readErr := conn.Read(data)
+		var data [512]byte
+		_, readErr := conn.Read(data[0:])
 
 		if readErr != nil {
 			fmt.Printf("Failed reading: %s", readErr)
@@ -44,11 +49,22 @@ func (b Bot) serverPump() {
 		if len(data) != 0 {
 			fmt.Printf("Server: %s\n", data)
 		} else {
-			shouldLoop = false
+			//shouldLoop = false
 		}
 
 	}
 
 	fmt.Println("Closing connection...")
 	conn.Close()
+}
+
+func (b Bot) sendRawCommand(command string, message string) {
+
+	var commandString = command
+	if message != "" {
+		commandString = commandString + " " + message
+	}
+
+	b.server.Write([]byte(commandString + "\r\n"))
+	fmt.Println("-> " + commandString)
 }
